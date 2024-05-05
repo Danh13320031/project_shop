4811d79ab959c9261dc97f099580e740d10dea80
Danh13320031 <danh13320031@gmail.com>
2024-05-05 23:47:37 +0700

    Filter product by price

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { styled } from '@mui/system';
 
-FilterByPrice.propTypes = {};
+const BoxCom = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(2),
+}));
 
-function FilterByPrice(props) {
-  return <div>Filter By Price</div>;
+const BoxCom2 = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  alignItems: 'center',
+
+  margin: `${theme.spacing(1)} 0px`,
+}));
+
+const TextFieldCom = styled(TextField)(({ theme }) => ({
+  input: {
+    padding: `${theme.spacing(0.5)} 14px`,
+  },
+}));
+
+const SpanTag = styled('span')(({ theme }) => ({
+  margin: `0px ${theme.spacing(1)}`,
+}));
+
+FilterByPrice.propTypes = {
+  onChange: PropTypes.func,
+};
+
+function FilterByPrice({ onChange }) {
+  const [values, setValues] = useState({
+    salePrice_gte: 0,
+    salePrice_lte: 0,
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (values.salePrice_gte === '' || values.salePrice_lte === '') return;
+    if (values.salePrice_gte < 0 || values.salePrice_lte < 0) return;
+    if (values.salePrice_lte < values.salePrice_gte) return;
+
+    if (onChange) onChange(values);
+  };
+
+  return (
+    <BoxCom>
+      <Typography variant="subtitle2" fontWeight={'bold'}>
+        CHỌN KHOẢNG GIÁ SẢN PHẨM
+      </Typography>
+
+      <BoxCom2>
+        <TextFieldCom name="salePrice_gte" value={values.salePrice_gte} onChange={handleChange} />
+        <SpanTag>-</SpanTag>
+        <TextFieldCom name="salePrice_lte" value={values.salePrice_lte} onChange={handleChange} />
+      </BoxCom2>
+
+      <Button variant="outlined" color="primary" onClick={handleSubmit}>
+        Áp dụng
+      </Button>
+    </BoxCom>
+  );
 }
 
 export default FilterByPrice;
